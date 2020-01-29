1845d7b17d01c2fc43383c573c5e3c7d20be72b1
user <user@users-Air.guest.substantial.com>
2020-01-29 11:03:30 -0800

    intiail commit v2

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-as"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUhttps://s4fquy86w6.execute-api.us-east-2.amazonaws.com/prod/notes"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_5EuoXlTCt",
+      APP_CLIENT_ID: "5u4bft8fmlr0ovh47s1kmttdv7",
+      IDENTITY_POOL_ID: "us-east-2:679827f8-95bc-4400-a781-2307278a9f3b"
+    }
+  };
---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+    retun (
+        <div className="NotFound">
+            <h3>Sorry, page <bold>not</bold> found!</h3>
+        </div>
+    );
+}
